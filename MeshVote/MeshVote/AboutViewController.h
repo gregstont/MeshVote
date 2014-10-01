dae8f091a1ea1ecf08e27e7799ea9d7d74174b62
gregstont <gregstont@gmail.com>
2014-10-01 15:38:41 -0500

    updated colors and about screen

---
+++ MeshVote/MeshVote/AboutViewController.h
@@ -0,0 +1,15 @@
+//
+//  AboutViewController.h
+//  MeshVote
+//
+//  Created by Taylor Gregston on 10/1/14.
+//  Copyright (c) 2014 Taylor Gregston. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+- (IBAction)githubButton:(id)sender;
+
+@end
--- MeshVote/MeshVote/Colors.m
+++ MeshVote/MeshVote/Colors.m
@@ -18,19 +18,19 @@ NSArray* letters;
 -(id)init {
     self = [super init];
     if(self) {
-        colors =      @[ [[UIColor alloc] initWithRed:0.258 green:0.756 blue:0.631 alpha:1.0], //green
-                         [[UIColor alloc] initWithRed:0 green:0.592 blue:0.929 alpha:1.0], //blue
-                         [[UIColor alloc] initWithRed:0.905 green:0.713 blue:0.231 alpha:1.0], //yellow
-                         [[UIColor alloc] initWithRed:1 green:0.278 blue:0.309 alpha:1.0], //red
-                         [[UIColor alloc] initWithRed:88.0/255 green:86.0/255 blue:214.0/255 alpha:1.0], //purple
-                         [[UIColor alloc] initWithRed:1 green:149.0/255 blue:0 alpha:1.0] ]; //orange
+        colors =      @[ [UIColor colorWithRed:0.258 green:0.756 blue:0.631 alpha:1.0], //green
+                         [UIColor colorWithRed:0 green:0.592 blue:0.929 alpha:1.0], //blue
+                         [UIColor colorWithRed:0.905 green:0.713 blue:0.231 alpha:1.0], //yellow
+                         [UIColor colorWithRed:1 green:0.278 blue:0.309 alpha:1.0], //red
+                         [UIColor colorWithRed:88.0/255 green:86.0/255 blue:214.0/255 alpha:1.0], //purple
+                         [UIColor colorWithRed:1 green:149.0/255 blue:0 alpha:1.0] ]; //orange
         
-        alphaColors = @[ [[UIColor alloc] initWithRed:0.258 green:0.756 blue:0.631 alpha:0.3], //green
-                         [[UIColor alloc] initWithRed:0 green:0.592 blue:0.929 alpha:0.3], //blue
-                         [[UIColor alloc] initWithRed:0.905 green:0.713 blue:0.231 alpha:0.3], //yellow
-                         [[UIColor alloc] initWithRed:1 green:0.278 blue:0.309 alpha:0.3], //red
-                         [[UIColor alloc] initWithRed:88.0/255 green:86.0/255 blue:214.0/255 alpha:0.3], //purple
-                         [[UIColor alloc] initWithRed:1 green:149.0/255 blue:0 alpha:0.3] ]; //orange
+        alphaColors = @[ [UIColor colorWithRed:0.258 green:0.756 blue:0.631 alpha:0.3], //green
+                         [UIColor colorWithRed:0 green:0.592 blue:0.929 alpha:0.3], //blue
+                         [UIColor colorWithRed:0.905 green:0.713 blue:0.231 alpha:0.3], //yellow
+                         [UIColor colorWithRed:1 green:0.278 blue:0.309 alpha:0.3], //red
+                         [UIColor colorWithRed:88.0/255 green:86.0/255 blue:214.0/255 alpha:0.3], //purple
+                         [UIColor colorWithRed:1 green:149.0/255 blue:0 alpha:0.3] ]; //orange
         letters = @[@"A", @"B", @"C", @"D", @"E", @"F", @"G"];
     }
     return self;
